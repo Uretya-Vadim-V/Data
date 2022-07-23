af2d589c8c4b121cba8af94a1a33ffdc003d89cf
Vadim <u.vadim238dapr@gmail.com>
2022-07-23 17:04:54 +0300

    Добавьте файлы проекта.

---
+++ Data/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Tree<int, int, string> tree = new(0);
+                using (StreamReader file = new(@"..\..\..\..\\data.txt"))
+                {
+                    string line;
+                    string[] stringData = new string[3];
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        stringData = line.Split(new char[] { ';' });
+                        tree.Add(Convert.ToInt32(stringData[0]), Convert.ToInt32(stringData[1]), stringData[2]);
+                    }
+                }
+
+                tree.BuildTrees();
+                foreach (var item in tree)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (FileNotFoundException filEx)
+            {
+                Console.WriteLine($"Файл не найден: {filEx.Message}");
+            }
+            catch (DirectoryNotFoundException dirEx)
+            {
+                Console.WriteLine($"Каталог не найден: {dirEx.Message}");
+            }
+        }
+    }
+}
